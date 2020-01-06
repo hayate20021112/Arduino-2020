8676802e8fd8536717943a52e3ae46c04da298a5
hayate <hayate@hayatenoMacBook-Air.local>
2020-01-06 16:11:04 +0900

    first commit

--- Arduino講習#1/PMS03/PMS03/PMS03.ino
+++ Arduino講習#1/PMS03/PMS03/PMS03.ino
@@ -1,17 +1,18 @@
-const int LED = 11;
-int val = 0;
+const int LED = 11;             //LEDが繋がってるピン
+int val = 0;                    //センサの値を記憶する変数
 
 void setup() {
-    pinMode(LED, OUTPUT);
+    pinMode(LED, OUTPUT);       //LEDのピンを出力に設定
+                                //アナログピンは自動で入力になる
 }
 
 void loop() {
-    val = analogRead(0);
+    val = analogRead(0);        //センサ（A0ピン）から値を読み込む
 
-    digitalWrite(LED, HIGH);
-    delay(val);
+    digitalWrite(LED, HIGH);    //LEDをオン
+    delay(val);                 //valの分プログラムを停止
 
-    digitalWrite(LED, LOW);
-    delay(val);
+    digitalWrite(LED, LOW);     //LEDをオン
+    delay(val);                 //valの分プログラムを停止
 
 }
